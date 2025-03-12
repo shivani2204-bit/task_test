b28d9c52495fb2869500cdac08746cdf9295ae64
shivani <jaiswalshivani.vhits@gmail.com>
2025-03-12 11:31:36 +0530

    test vercel3

--- api/server.js
+++ api/server.js
@@ -1,17 +1,20 @@
 import express from "express"
-const app  = express()
+import dotenv from "dotenv"
+import cors from "cors"
+
 import connectDb from "../src/config/ConfigDb.js"
 import userRouter from "../src/routes/UserRoute.js"
 import taskRouter from "../src/routes/TaskRoute.js"
 import projectRouter from "../src/routes/ProjectRoute.js"
 import adminRouter from "../src/routes/AdminRoute.js"
-import dotenv, { config } from "dotenv"
-import cors from "cors"
 
+const app = express()
 dotenv.config()
 app.use(cors());
 app.use(express.json())
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
+
+connectDb()
 
 // app.use("/user",userRouter)
 // app.use("/task",taskRouter)
@@ -22,9 +25,13 @@ app.get('/', (req, res) => {
   res.send("hello world of testing the deployment on vercel...");
 });
 
-let server = app.listen(port,()=>{
-    connectDb()
-    console.log("server is running on port 9000")
-})
+// let server = app.listen(port,()=>{
+//     connectDb()
+//     console.log("server is running on port 9000")
+// })
 
-export default server;
+// export default server;
+
+app.listen(port, () => {
+  console.log(`app listen on ${port}`);
+});
